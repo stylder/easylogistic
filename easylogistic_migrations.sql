8eafb3a87691442fb42bae56214a40d0c9f5cb2f
stylder <stylder@gmail.com>
2017-04-07 17:59:05 -0500

    Imagenes funciona subir a carpeta unidades

--- easylogistic_migrations.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO easylogistic.migrations (migration, batch) VALUES ('2014_10_12_000000_create_users_table', 1);
-INSERT INTO easylogistic.migrations (migration, batch) VALUES ('2014_10_12_100000_create_password_resets_table', 1);
-INSERT INTO easylogistic.migrations (migration, batch) VALUES ('2017_04_03_171338_CrearBaseDatos', 1);
